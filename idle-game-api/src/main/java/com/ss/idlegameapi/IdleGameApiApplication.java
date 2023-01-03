f4b4cf72a5c5693d45533aae9b2fe1caaec203b4
stiaselv <55443662+stiaselv@users.noreply.github.com>
2023-01-03 01:36:46 +0100

    first commit

---
+++ idle-game-api/src/main/java/com/ss/idlegameapi/IdleGameApiApplication.java
@@ -0,0 +1,13 @@
+package com.ss.idlegameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdleGameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdleGameApiApplication.class, args);
+	}
+
+}
